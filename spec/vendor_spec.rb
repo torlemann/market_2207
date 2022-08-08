c08c8243dff1003ead26c9f214db3ceaf0e3b6b4
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-08 09:41:08 -0700

    Move potential revenue test from market to vendor

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -6,23 +6,33 @@ RSpec.describe Vendor do
   let(:item2) {Item.new({name: 'Tomato', price: '$0.50'})}
   let(:vendor) {Vendor.new("Rocky Mountain Fresh")}
 
-  it 'exists and has readable attributes' do
-    expect(vendor).to be_a Vendor
-    expect(vendor.name).to eq "Rocky Mountain Fresh"
-    expect(vendor.inventory).to eq({})
+  describe 'Iteration 1' do
+    it 'exists and has readable attributes' do
+      expect(vendor).to be_a Vendor
+      expect(vendor.name).to eq "Rocky Mountain Fresh"
+      expect(vendor.inventory).to eq({})
+    end
+
+    it 'can check stock' do
+      expect(vendor.check_stock(item1)).to eq 0
+    end
+
+    it 'can add stock and return inventory' do
+      vendor.stock(item1, 30)
+      expect(vendor.inventory).to eq({item1 => 30})
+      expect(vendor.check_stock(item1)).to eq 30
+      vendor.stock(item1, 25)
+      expect(vendor.check_stock(item1)).to eq 55
+      vendor.stock(item2, 12)
+      expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
+    end
   end
 
-  it 'can check stock' do
-    expect(vendor.check_stock(item1)).to eq 0
-  end
-
-  it 'can add stock and return inventory' do
-    vendor.stock(item1, 30)
-    expect(vendor.inventory).to eq({item1 => 30})
-    expect(vendor.check_stock(item1)).to eq 30
-    vendor.stock(item1, 25)
-    expect(vendor.check_stock(item1)).to eq 55
-    vendor.stock(item2, 12)
-    expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
+  describe 'Iteration 2' do
+    it 'can the sum of all prices of items by vendor' do
+      expect(vendor1.potential_revenue).to eq 29.75
+      expect(vendor2.potential_revenue).to eq 345.00
+      expect(vendor3.potential_revenue).to eq 48.75
+    end
   end
 end
