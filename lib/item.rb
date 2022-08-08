1041234d470d39bcd18f1169e07728b25633969d
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-08 08:49:51 -0700

    Add initialize to item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,8 @@
 class Item
+
+  attr_reader :name, :price
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @price = attributes.fetch(:price).delete("$").to_f
+  end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Item do
   let(:item2) {Item.new({name: 'Tomato', price: '$0.50'})}
 
   it 'exists and has readable attributes' do
-    expect(item).to be_a Item
+    expect(item2).to be_a Item
     expect(item2.name).to eq "Tomato"
     expect(item2.price).to eq (0.50)
   end
