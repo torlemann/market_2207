3fc66a9c8a667324a5c920a723b9fb3796ef648e
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-08 09:01:21 -0700

    Add check stock method to vendor class

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -18,11 +18,11 @@ RSpec.describe Vendor do
 
   it 'can add stock and return inventory' do
     vendor.stock(item1, 30)
-    expect(vendor.inventory).to eq {item1 => 30}
+    expect(vendor.inventory).to eq({item1 => 30})
     expect(vendor.check_stock(item1)).to eq 30
     vendor.stock(item1, 25)
     expect(vendor.check_stock(item1)).to eq 55
     vendor.stock(item2, 12)
-    expect(vendor.inventory).to eq {item1 => 30, items => 12}
+    expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
   end
 end
